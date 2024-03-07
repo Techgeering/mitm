eaaf2cd610b65f6d7809db428999ca1a4cda8a27
Ritik Rosan Sahoo <105884612+ritiksahoors@users.noreply.github.com>
2024-03-07 12:43:15 +0530

    media query

--- about.php
+++ about.php
@@ -31,7 +31,7 @@
 
 	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css?v=1.6">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css?v=<?php  echo time(); ?>"/>
 
 	<!--toaster alert-->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
