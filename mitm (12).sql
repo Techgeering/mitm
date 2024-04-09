3481ca53325868c7c9b943eee188502a5fc562fb
sarmisthadas065 <sarmisthadas065@gmail.com>
2024-04-09 09:58:53 +0530

    add db

--- mitm (12).sql
+++ mitm (12).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2024 at 08:45 AM
+-- Generation Time: Apr 09, 2024 at 06:26 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `mitm`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mitm_campus_placement`
+--
+
+CREATE TABLE `mitm_campus_placement` (
+  `id` int(11) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `year` int(10) NOT NULL,
+  `branch` longtext NOT NULL,
+  `student_name` varchar(50) NOT NULL,
+  `company_name` longtext NOT NULL,
+  `date_of_upload` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mitm_campus_placement`
+--
+
+INSERT INTO `mitm_campus_placement` (`id`, `image`, `year`, `branch`, `student_name`, `company_name`, `date_of_upload`, `status`) VALUES
+(9, '6613db6fc2097.jpg', 2022, 'COMPUTER SCIENCE ENGINEERING', 'sarmistha das', 'techgeering', '2024-04-08 11:56:31', 1);
 
 -- --------------------------------------------------------
 
@@ -84,6 +108,33 @@ INSERT INTO `mitm_enquiry` (`id`, `enquiry_name`, `enquiry_email`, `enquiry_mobi
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `mitm_faculty`
+--
+
+CREATE TABLE `mitm_faculty` (
+  `id` int(11) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `faculty_name` varchar(50) NOT NULL,
+  `designation` mediumtext NOT NULL,
+  `status` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mitm_faculty`
+--
+
+INSERT INTO `mitm_faculty` (`id`, `image`, `faculty_name`, `designation`, `status`) VALUES
+(7, '6613cad454834.jpg', 'Mr. Binayak Sahoo', 'HOD OF COMPUTER SCIENCE & ENGINEERING', 1),
+(8, '6613caf60fdae.jpg', 'Mr. Suryapratap Biswajit Puhan', 'HOD OF CIVIL ENGINEERING', 1),
+(9, '6613cb25d77d7.jpg', 'Mrs. Shradhanjali Panda', 'HOD OF ELECTRONICS AND COMMUNICATION ENGINEERING', 1),
+(10, '6613deb8282f7.jpg', 'Mr. Debasish Hota', 'HOD OF ELECTRICAL AND ELECTRONICS ENGINEERING', 1),
+(11, '6613cb78c8f8d.jpg', 'Mr. Girija Sankar Panigrahi', 'HOD OF ELECTRICAL ENGINEERING', 1),
+(12, '6613ddf67fb27.jpg', 'Mr. Naresh Kumar Ojha', 'HOD OF MBA', 1),
+(13, '6613d99f4f5d6.jpg', 'Mr. Ajit Kumar Dhal', 'HOD OF MECHANICAL ENGINEERING', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `mitm_life_image`
 --
 
@@ -101,8 +152,6 @@ CREATE TABLE `mitm_life_image` (
 INSERT INTO `mitm_life_image` (`id`, `image`, `date_of_upload`, `status`) VALUES
 (1, '65bc7c605570e.jpg', '2024-02-08 04:07:48', 0),
 (2, '65bc7cc4c8f8f.jpg', '2024-02-08 04:07:51', 0),
-(3, '65bc7ffe4feaa.jpg', '2024-02-08 04:07:55', 0),
-(5, '65bc8015b8e2e.jpg', '2024-02-08 04:05:30', 1),
 (8, '65c0aa79b7ac9.jpg', '2024-02-05 09:29:29', 1),
 (9, '65c45387b0c09.jpg', '2024-02-08 04:07:35', 1),
 (10, '65c4585f8ad55.jpg', '2024-02-08 04:28:15', 1),
@@ -197,11 +246,11 @@ CREATE TABLE `mitm_placement` (
 --
 
 INSERT INTO `mitm_placement` (`id`, `image`, `date_of_upload`, `status`) VALUES
-(1, '65c5abca08357.png', '2024-02-09 04:36:36', 1),
 (2, '65c5ac0638a55.png', '2024-02-09 05:21:49', 0),
 (3, '65c5b63633713.png', '2024-02-09 05:20:54', 1),
 (4, '65c5d63f57837.jpeg', '2024-02-09 07:37:35', 1),
-(5, '65c5ffafcc434.jpeg', '2024-02-09 10:34:23', 1);
+(5, '65c5ffafcc434.jpeg', '2024-02-09 10:34:23', 1),
+(6, '6613d75128f28.png', '2024-04-08 11:38:57', 1);
 
 -- --------------------------------------------------------
 
@@ -227,6 +276,12 @@ INSERT INTO `mitm_user` (`id`, `mitm_user_username`, `mitm_user_password`) VALUE
 --
 
 --
+-- Indexes for table `mitm_campus_placement`
+--
+ALTER TABLE `mitm_campus_placement`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `mitm_contact`
 --
 ALTER TABLE `mitm_contact`
@@ -236,6 +291,12 @@ ALTER TABLE `mitm_contact`
 -- Indexes for table `mitm_enquiry`
 --
 ALTER TABLE `mitm_enquiry`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mitm_faculty`
+--
+ALTER TABLE `mitm_faculty`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -279,6 +340,12 @@ ALTER TABLE `mitm_user`
 --
 
 --
+-- AUTO_INCREMENT for table `mitm_campus_placement`
+--
+ALTER TABLE `mitm_campus_placement`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT for table `mitm_contact`
 --
 ALTER TABLE `mitm_contact`
@@ -289,6 +356,12 @@ ALTER TABLE `mitm_contact`
 --
 ALTER TABLE `mitm_enquiry`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `mitm_faculty`
+--
+ALTER TABLE `mitm_faculty`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `mitm_life_image`
@@ -318,7 +391,7 @@ ALTER TABLE `mitm_notice`
 -- AUTO_INCREMENT for table `mitm_placement`
 --
 ALTER TABLE `mitm_placement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `mitm_user`
