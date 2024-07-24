d10100d11bd70b86194504a70222a72fc65c7393
sarmisthadas065 <sarmisthadas065@gmail.com>
2024-07-24 19:00:12 +0530

    Update faculty.php

--- faculty.php
+++ faculty.php
@@ -40,10 +40,10 @@
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
-	<title>MITM | About Us</title>
+	<!-- <title>MITM | About Us</title> -->
 	<style>
-		@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-		@import url("https://use.fontawesome.com/releases/v5.13.0/css/all.css");
+		/* @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+		@import url("https://use.fontawesome.com/releases/v5.13.0/css/all.css"); */
 
 		:root {
 			--font3: 'Roboto', sans-serif;
