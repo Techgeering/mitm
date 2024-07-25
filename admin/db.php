6c13e7a18280557150f5783d65c8dc182d827035
sarmisthadas065 <sarmisthadas065@gmail.com>
2024-07-25 14:53:50 +0530

    Update db.php

--- admin/db.php
+++ admin/db.php
@@ -1,15 +1,15 @@
 <?php
     $servername="localhost";
-    // $username="u728233529_nbs";
-    // $password=";hbq73#i/?Z7";
-    // $databasename="u728233529_nbs";
-    // $base_url="https://mitm.edu.in/";
+    $username="u728233529_nbs";
+    $password=";hbq73#i/?Z7";
+    $databasename="u728233529_nbs";
+    $base_url="https://mitm.edu.in/";
 
     
-    $username="root";
-    $password="";
-    $databasename="mitm";
-    $base_url="https://localhost/mitm/";
+    // $username="root";
+    // $password="";
+    // $databasename="mitm";
+    // $base_url="https://localhost/mitm/";
     $conn=new mysqli($servername,$username,$password,$databasename);
     if($conn->connect_error){
         die($conn->connect_error);
